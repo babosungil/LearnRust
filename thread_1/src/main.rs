dedd60a1d3a78d812f1a2e5d605b7f6cef8487a6
sungil <babosungil@gmail.com>
2024-07-08 17:52:39 +0900

    Baisc _ var, data type

--- thread_1/src/main.rs
+++ thread_1/src/main.rs
@@ -1,11 +1,7 @@
-//use std::string;
-
+use std::thread;
 
 fn main() {
     let mut var_mut = 1;        // mutable
-    println!("{var_mut}");
-    var_mut = 2;
-    println!("{var_mut}");
 
     let var = 2;                // immutable
     println!("{var}");
@@ -22,15 +18,16 @@ fn main() {
     const TEST_CONSTANT_VALUE:i32 = 10 * 20 / 5;    // constant
     println!("{TEST_CONSTANT_VALUE}");
 
-    let integer32:i32 = 9876;
-    println!("{integer32}");
 
-    let mut integer8:i8 = 125;
-    integer8 += 1;
-    println!("{integer8}");
-    //integer8 += 127;                    //overflow    
 
-    let tuple_test: (i8, f32, char, String) = (12, 3.4, 'a', ("string_test").to_string());
-    println!("{:?}", tuple_test);
 
+
+
+
+    //thread::spawn(test_func(||{1}));
+    //thread::spawn(test_func(||{2}));
+}
+
+fn test_func(param_1 : i32) {
+    println!("call test_func with param {param_1}");
 }
