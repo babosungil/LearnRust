e78bceb6d3fdb771aff8444a39f52c99d09791bd
sungil <babosungil@gmail.com>
2024-07-11 14:47:47 +0900

    reference, mutable reference

--- basic_ownership/src/main.rs
+++ basic_ownership/src/main.rs
@@ -34,4 +34,39 @@ fn main() {
     let copy_4 = copy_3;
     println!("copy : {:?}, {:?}", copy_3, copy_4);
 
+
+    // with function
+    let func_1 = 321;
+    let func_2 = String::from("with function");
+    func(func_1, func_2.clone());
+    println!("func_2 : {func_2}");
+    let func_3 = func(func_1, func_2);
+    //println!("func_2 : {func_2}");        // error
+    println!("func_3 : {func_3}");
+
+
+    // reference
+    let ref_1 = String::from("reference");
+    let mut ref_2 = String::from("reference2");    
+    func_ref(&ref_1, &mut ref_2);
+    
+    //func_ref(&ref_2, &mut ref_2); // error
+    println!("ref_2 : {ref_2}");
+
+    /* error - mutable borrow - able only one
+    let ref_3 = &mut ref_2;
+    let ref_4 = &mut ref_2;
+    println!("{ref_3}, {ref_4}");
+    */
+}
+
+fn func(param_1 : i32, mut param_2 : String) -> String {
+    println!("call func - param_1 : {param_1}, param_2 : {param_2}");
+    param_2.push_str(", push_str");
+    param_2
+}
+
+fn func_ref(param_1 : &String, param_2 : &mut String) {
+    println!("call func_ref - param_1 : {param_1}, param_2 : {param_2}");
+    param_2.push_str(", push_str");
 }
