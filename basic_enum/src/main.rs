71ace2ae8ea9437dabdfdc77f5f7bace01d46748
sungil <babosungil@gmail.com>
2024-07-15 16:02:45 +0900

    enum

--- basic_enum/src/main.rs
+++ basic_enum/src/main.rs
@@ -7,6 +7,13 @@ enum EEnumTest {
     StructTest { a : i32, b : String },
 }
 
+#[derive(Debug)]
+enum EOption<T> {
+    None,
+    Some(T)
+
+}
+
 fn main() {
     let enumTest = EEnumTest::EnumBase;    
     println!("enumTest {:?}", enumTest);
@@ -16,4 +23,14 @@ fn main() {
     println!("stringTest {:?}", stringTest);
     let structTest = EEnumTest::StructTest { a: 222, b: String::from("struct test") };
     println!("structTest {:?}", structTest);
+
+
+    let optionSome_i32 = EOption::Some(123);
+    println!("optionSome_i32 : {:?}", optionSome_i32);
+    let optionSome_String = EOption::Some(String::from("Optine - Some"));
+    println!("optionSome_String : {:?}", optionSome_String);
+    let optionSome_Char = EOption::Some('A');
+    println!("optionSome_Char : {:?}", optionSome_Char);
+
+    //let temp = 1 + optionSome_i32; // error - data type miss match
 }
