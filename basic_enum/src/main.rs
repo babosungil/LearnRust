150c4fec1f00203af377e2b080fc10c814cfb282
sungil <babosungil@gmail.com>
2024-07-16 14:45:30 +0900

    enum option match

--- basic_enum/src/main.rs
+++ basic_enum/src/main.rs
@@ -8,29 +8,90 @@ enum EEnumTest {
 }
 
 #[derive(Debug)]
-enum EOption<T> {
+enum NewOption<T> {
     None,
     Some(T)
-
 }
 
+fn match_option(a : Option<i32>) -> Option<i32> {
+    match a {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+#[derive(PartialEq)]
+enum ETeam {
+    Team1 = 1,
+    Team2 = 2,
+    Team3 = 3,
+}
+
+enum ESport {
+    Soccer,
+    Baseball(ETeam),
+    Basketball,
+    Volleyball,
+}
+
+fn match_sport(sport : ESport) -> u8 {
+    match sport {
+        ESport::Soccer => 1,        
+        ESport::Baseball(team) => {                    
+            if team == ETeam::Team1 {
+                2
+            }
+            else if team == ETeam::Team2 {
+                3
+            }            
+            else if team == ETeam::Team3 {
+                4
+            }
+            else {
+                5
+            }
+        },
+        ESport::Basketball => 8,
+        ESport::Volleyball => {
+            println!("Volleyball !!!");
+            16
+        },
+    }
+}
+
+
 fn main() {
-    let enumTest = EEnumTest::EnumBase;    
-    println!("enumTest {:?}", enumTest);
-    let tupleTest = EEnumTest::TupleTest(1, String::from("hello?"));
-    println!("tupleTest {:?}", tupleTest);
-    let stringTest = EEnumTest::StringTest(String::from("rust!"));
-    println!("stringTest {:?}", stringTest);
-    let structTest = EEnumTest::StructTest { a: 222, b: String::from("struct test") };
-    println!("structTest {:?}", structTest);
+    let enum_test = EEnumTest::EnumBase;    
+    println!("enum_test {:?}", enum_test);
+    let tuple_test = EEnumTest::TupleTest(1, String::from("hello?"));
+    println!("tuple_test {:?}", tuple_test);
+    let string_test = EEnumTest::StringTest(String::from("rust!"));
+    println!("string_test {:?}", string_test);
+    let struct_test = EEnumTest::StructTest { a: 222, b: String::from("struct test") };
+    println!("struct_test {:?}", struct_test);
 
 
-    let optionSome_i32 = EOption::Some(123);
-    println!("optionSome_i32 : {:?}", optionSome_i32);
-    let optionSome_String = EOption::Some(String::from("Optine - Some"));
-    println!("optionSome_String : {:?}", optionSome_String);
-    let optionSome_Char = EOption::Some('A');
-    println!("optionSome_Char : {:?}", optionSome_Char);
+    let option_i32 = NewOption::Some(123);
+    println!("option_i32 : {:?}", option_i32);
+    let option_string = NewOption::Some(String::from("Optine - Some"));
+    println!("option_string : {:?}", option_string);
+    let option_char = NewOption::Some('A');
+    println!("option_char : {:?}", option_char);
+
+    let option_1 = Some(123);
+    let option_2 = match_option(option_1);
+    println!("option_1 : {:?}, option_2 : {:?}", option_1, option_2);
 
     //let temp = 1 + optionSome_i32; // error - data type miss match
+
+
+    let soccer_value = match_sport(ESport::Soccer);
+    let basketball_value = match_sport(ESport::Basketball);
+    println!("Soccer : {soccer_value}, Basketball : {basketball_value}");
+
+    let volleyball_value = match_sport(ESport::Volleyball);
+    println!("Volleyball : {volleyball_value}");
+
+    let baseball_value = match_sport(ESport::Baseball(ETeam::Team2));
+    println!("Baseball = Team2 : {baseball_value}");
 }
