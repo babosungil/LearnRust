dc952ee1b5844d672912333630ccdf2cd1fc934b
sungil <babosungil@gmail.com>
2024-07-29 16:54:16 +0900

    panic, result

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,12 +1,33 @@
 use core::panic;
-use std::{fs::File, io::ErrorKind, ptr::{null, NonNull}};
+use std::{fs::{self, File}, io::{self, ErrorKind, Read, Write}, ptr::{null, NonNull}};
 
-/*
-enum Result<T,E> {
-    Ok(T),
-    Error(E),
+fn read_propagating() -> Result<String, io::Error> {
+    let result = File::open("propagating.txt");
+    let mut open_file = match result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut read_text = String::new();
+    match open_file.read_to_string(&mut read_text) {
+        Ok(_) => Ok(read_text),
+        Err(e) => Err(e),
+    }
 }
-*/
+
+
+fn read_propagating_with_qm() -> Result<String, io::Error> {
+    let mut result = File::open("propagating.txt")?;
+    let mut read_text = String::new();
+    result.read_to_string(&mut read_text)?;
+    Ok(read_text)
+}
+
+fn read_propagating_simple() -> Result<String, io::Error> {
+    fs::read_to_string("propagating.txt")
+}
+
+
 
 fn main() {
     //panic!("panic!");     // occur panic
@@ -33,4 +54,21 @@ fn main() {
     };
 
     //let file_open_result = File::open("new_file_2.txt").expect("file open exception !!");
+
+
+    let read_text = read_propagating();
+    if read_text.is_ok() {
+        println!("file data : {}", read_text.unwrap());
+    }
+    else {
+        println!("{:?}", read_text.err());
+    }
+
+    let read_text_qm = read_propagating_with_qm();
+    println!("file data qm {}", read_text_qm.unwrap());
+
+    let read_test_simple = read_propagating_simple();
+    println!("file data simple {}", read_test_simple.unwrap());
+
 }
+
