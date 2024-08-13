188576c0243890c7bf819af6801a4aaf993f8de1
sungil <babosungil@gmail.com>
2024-08-13 17:09:22 +0900

    cargo doc

---
+++ test_cargo/src/lib.rs
@@ -0,0 +1,17 @@
+/// My Test Function
+/// 
+/// # HowTO !
+/// let result = test(2)
+/// 
+/// # Panics
+/// Panics Example!
+/// 
+/// # Errors
+/// This is Error 
+/// 
+/// # Safety
+/// safety!?
+pub fn test(a : i32) -> i32 {
+    a * a
+}
+
