0b7ce0590fb7ffdf5cfdee210928ddfea4675549
sungil <babosungil@gmail.com>
2024-08-23 12:30:12 +0900

    ref cell

--- pointer/src/main.rs
+++ pointer/src/main.rs
@@ -1,3 +1,6 @@
+use std::borrow::{Borrow, BorrowMut};
+use std::rc::Rc;
+use std::cell::RefCell;
 
 enum MyList {
     Cons(i32, Box<MyList>),
@@ -11,16 +14,18 @@ enum MyRcList {
 
 use crate::MyList::{Cons, Nil};
 use crate::MyRcList::{ConsRc, NilRc};
-use std::rc::Rc;
 
-// reference counting
-//Rc<T>
+struct RefCellTest {
+    vvv : i32,
+}
+
 
 fn main() {
     // box new 
     let a = Cons(1, Box::new(Cons(11, Box::new(Nil))));
     let b = Cons(2, Box::new(a));
     //let c = Cons(3, Box::new(a));   // error - value moved prev line
+
 
     // rc new, clone
     let aa = Rc::new(ConsRc(4, Rc::new(ConsRc(11, Rc::new(NilRc)))));
@@ -34,9 +39,16 @@ fn main() {
     println!("4 reference count : {}", Rc::strong_count(&aa));
 
 
-
     let aaa = Rc::new(ConsRc(4, Rc::new(ConsRc(11, Rc::new(NilRc)))));
     println!("1 weak count : {}", Rc::weak_count(&aa));
     let bbb = ConsRc(12, Rc::clone(&aa));
-    println!("2 weak count : {}", Rc::weak_count(&aa));    
+    println!("2 weak count : {}", Rc::weak_count(&aa));   
+    
+
+    let ref_test = RefCell::new(RefCellTest{ vvv : 999 });
+    let ref_test_1 = ref_test.borrow();
+    
+    let mut borrow_mut = ref_test.borrow_mut();     // runtime error!
+    borrow_mut.vvv += 1;
+
 }
