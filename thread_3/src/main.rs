b4a32e5e70d8220d0f6d192097f51fae557428d3
sungil <babosungil@gmail.com>
2024-09-05 18:55:16 +0900

    mutex test

--- thread_3/src/main.rs
+++ thread_3/src/main.rs
@@ -20,11 +20,52 @@ fn main() {
                 let mut mutex_guard = mutex.lock().unwrap();
                 for _ in 0..1000 {
                     *mutex_guard += 1;
-                }                
+                }
             });
             thread::sleep(Duration::from_millis(1));
-            println!("{}", mutex.lock().unwrap());
-        }        
+            println!("test 1 - {}", mutex.lock().unwrap());
+        }
+    });
+
+
+    
+    let mut vec_test = Vec::new();
+    vec_test.push(1);
+    let mutex = Mutex::new(vec_test);
+    scope(|s|{
+        for _ in 0..10 {
+            s.spawn(|| {
+
+                for _ in 0..10000 {
+                    mutex.lock().unwrap().push(1);
+                }
+                println!("test 2 - loop fin");
+            });
+
+            thread::sleep(Duration::from_millis(1));
+            println!("test 2 - {}", mutex.lock().unwrap().len());
+        }
+    });
+
+
+
+    scope(|s|{
+        println!("test 3 start");
+        for i in 0..10 {
+            println!("test 3 start id : {i}");
+            s.spawn(|| {
+
+                for _ in 0..10000 {
+                    if let a = mutex.lock().unwrap().len() > 1 {
+                        mutex.lock().unwrap().pop();                        // not release mutex guard
+                    }
+                }
+                println!("test 3 - loop fin");
+            });
+
+            thread::sleep(Duration::from_millis(1));
+            println!("test 3 - {}", mutex.lock().unwrap().len());
+        }
     });
 }
 
