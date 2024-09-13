c74468d94c70d78389efea21f24353ada37a9db3
sungil <babosungil@gmail.com>
2024-09-13 12:29:10 +0900

    thread - fetch

--- thread_4/src/main.rs
+++ thread_4/src/main.rs
@@ -1,16 +1,18 @@
 use std::os::windows::thread;
-use std::sync::atomic::AtomicI32;
-use std::sync::atomic::AtomicI8;
-use std::sync::atomic::AtomicUsize;
-use std::sync::atomic::Ordering;
+use std::sync::atomic::{AtomicI32, AtomicI8, AtomicU64, AtomicUsize, Ordering};
 use std::thread::scope;
 use std::time::Duration;
 use std::thread::sleep;
 use core::sync::atomic::Ordering::Relaxed;
+use std::time::Instant;
 
 
-fn process(id : i32) {
-    sleep(Duration::from_millis(100));
+fn process(id : i32) -> u64 {
+    let mut counter = 0;
+    for _ in 0 .. id * 1000000 {
+        counter += 1;
+    }
+    return counter;
 }
 
 fn main() {
@@ -24,27 +26,58 @@ fn main() {
     println!("b: {b}, c: {c}");
 
 
+    
+    println!("=== start! ===");
+
     let checker = &AtomicUsize::new(0);
+    let time_total = &AtomicU64::new(0);
+    let time_max = &AtomicU64::new(0);
+
     scope(|s|{
         for i in 0..4 {
             s.spawn(move|| {
                 for k in 0..25 {
+                    let time_start = Instant::now();
                     process(i * 25 + k);
                     checker.fetch_add(1, Relaxed);
+                    let time_taken = time_start.elapsed().as_micros() as u64;
+
+                    time_total.fetch_add(time_taken, Relaxed);
+                    time_max.fetch_max(time_taken, Relaxed);
                 }
             });
         }
+
+        loop {
+            let n: usize = checker.load(Relaxed);
+            let total = Duration::from_micros(time_total.load(Relaxed));
+            let max = Duration::from_micros(time_total.load(Relaxed));
+            let mut avg = total;
+            if n > 0 {
+                avg = avg / n as u32;
+            }
+
+            println!("working.. {n}/100 done... average : {:?}, peak : {:?}", avg, max);
+            if n == 100 {
+                break;
+            }
+            sleep(Duration::from_millis(100));
+        }
     });
 
-    loop {
-        let n = checker.load(Relaxed);
-        if n == 100 {
-            break;
+    println!("=== fin!. ===");
+
+
+    
+    let checker_count = checker.load(Relaxed);
+    match checker.compare_exchange(checker_count, 200, Relaxed, Relaxed) {
+        Ok(_) => {
+            println!("checker_count compare success - 100");
+        },
+        Err(v) => {
+            println!("checker_count compare fail {v}");
         }
-
-        println!("working.. {n}/100 done");
-        sleep(Duration::from_secs(1));
     }
-
-    println!("fin!.");
+    let checker_count = checker.load(Relaxed);
+    println!("checker_count : {checker_count}");
 }
